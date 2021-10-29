19dfdf9f8533a441c6fa675ff840d8c1013d29b9
bronx92 <especialcontas@outlook.com>
2021-10-29 14:45:00 -0300

    Primeiro commit

---
+++ src/main/java/com/bronx92/personapi/PersonapiApplication.java
@@ -0,0 +1,13 @@
+package com.bronx92.personapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bronx92/personapi/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bronx92.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
